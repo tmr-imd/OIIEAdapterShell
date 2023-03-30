482eeaf5fc21bad7203f84cf4a7b938915d7a1cc
Matt Selway <30459857+mattys101@users.noreply.github.com>
2023-03-30 20:24:13 +1030

    Added check for applicable rule
    Not quite visualising the way I was hoping it would.

--- src/AdapterServer/Pages/ConfirmBODConfigViewModel.cs
+++ src/AdapterServer/Pages/ConfirmBODConfigViewModel.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using AdapterServer.Data;
 
 namespace AdapterServer.Pages
@@ -6,9 +7,28 @@ namespace AdapterServer.Pages
     {
         public ConfirmationSettings Config { get; set; } = new ConfirmationSettings(new List<ConfirmBODSetting>());
 
+        [Required(AllowEmptyStrings = false)]
+        [RegularExpression(@"^((\*)|(/[^/]+(/[^/]+)*))$", ErrorMessage = "The Channel URI must be either an asterisk or an absolute URI path")]
+        [Display(Name = "Channel URI")]
+        public string ChannelUriToCheck { get; set; } = "";
+
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Topic")]
+        public string TopicToCheck { get; set; } = "";
+
+        public string CheckResult { get; set; } = "";
+
         public async Task Setup(SettingsService settings)
         {
             Config = await settings.LoadSettings<ConfirmationSettings>("BODConfirmations");
+        }
+
+        public async Task CheckConfirmationRequired(SettingsService settings)
+        {
+            // Refresh in case changes have happened elsewhere
+            await Setup(settings);
+
+            CheckResult = Config.ConfirmationOptionFor(ChannelUriToCheck, TopicToCheck).ToString();
         }
     }
 }
